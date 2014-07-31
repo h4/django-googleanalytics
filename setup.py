b3109ce2311c27ce44cd84c0bafd5b6591339b75
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-07-31 22:30:40 +0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-googleanalytics',
+    version='0.1.0',
+    packages=['google_analytics', 'google_analytics.templatetags'],
+    url='https://github.com/h4/django-sociallinks/',
+    license='MIT',
+    author='Mikhail Baranov',
+    author_email='mkhl@brnv.ru',
+    description='Adding google analytics to django'
+)
